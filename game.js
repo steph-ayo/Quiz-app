58480b71edca6e5ce754d0ca9d7951186262c8af
steph-ayo <popoolastephen19@gmail.com>
2023-08-29 21:19:16 +0100

    initial commit

---
+++ game.js
@@ -0,0 +1,16 @@
+
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
